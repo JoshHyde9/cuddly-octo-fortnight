5bfd8b43c11da506df4e6a1b82e84d8871caa600
JoshHyde9 <joshhyde546@gmail.com>
2022-09-30 15:15:10 +1000

    feat: nav moves away on scroll down but comes back on scroll up

--- js/index.js
+++ js/index.js
@@ -9,3 +9,16 @@ const copyToClipboard = () => {
     toolTipText.innerText = "Click to copy email address to clipboard.";
   }, 3000);
 };
+
+let lastScrollTop;
+const navbar = document.getElementById("nav");
+window.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-80px";
+    navbar.classList.add("transition");
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
